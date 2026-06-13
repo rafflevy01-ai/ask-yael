c6caee93df4a66b48fbbe354facdd9088ae34eed
Raff Levy <rafflevy01@gmail.com>
2026-06-13 21:52:44 +0000

    File changes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,23 +6,16 @@ import HearYaelSection from "@/components/HearYaelSection";
 import HowItWorksSection from "@/components/HowItWorksSection";
 import CapabilitiesSection from "@/components/CapabilitiesSection";
 import FaqSection from "@/components/FaqSection";
-import SectionDividers, { SectionDivider } from "@/components/SectionDividers";
 
 export default function Home() {
   return (
     <main>
-      <SectionDividers />
       <HeroSection />
       <ProofBar />
-      <SectionDivider />
       <ProblemSection />
-      <SectionDivider />
       <HearYaelSection />
-      <SectionDivider />
       <HowItWorksSection />
-      <SectionDivider />
       <CapabilitiesSection />
-      <SectionDivider />
       <FaqSection />
     </main>
   );
